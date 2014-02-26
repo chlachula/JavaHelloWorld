36c182c55173b41e6320132b3967d751747cfb8a
chlachula <josefch@gmail.com>
2014-02-26 11:34:44 -0600

    First HelloWorld file

---
+++ src/com/minetlab/a/JavaHelloWorld.java
@@ -0,0 +1,22 @@
+/*
+ * Josef hlachula
+ * jch@minetlab.com
+ */
+
+package com.minetlab.a;
+
+/**
+ * HelloWorld on command line
+ * 
+ * @author Josef Chlachula
+ */
+public class JavaHelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+}

4005becb9fe96fdcf5243821fec870b21fbce0a9
chlachula <josefch@gmail.com>
2014-02-26 19:05:58 -0600

    GUI option

--- src/com/minetlab/a/JavaHelloWorld.java
+++ src/com/minetlab/a/JavaHelloWorld.java
@@ -1,22 +1,64 @@
 /*
- * Josef hlachula
+ * Josef Chlachula
  * jch@minetlab.com
  */
-
 package com.minetlab.a;
+
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 /**
  * HelloWorld on command line
- * 
+ *
  * @author Josef Chlachula
  */
 public class JavaHelloWorld {
+
+    public JavaHelloWorld() {
+        JFrame frame = new JFrame();
+
+        //Exit when the frame is closed
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Example GUI");
+        frame.setSize(150, 75);
+
+        //Center the JFrame to the middle of the screen
+        frame.setLocationRelativeTo(null);
+
+        JLabel listLbl = new JLabel("Hello World!");
+        frame.add(listLbl, BorderLayout.CENTER);
+
+        frame.setVisible(true);
+    }
+
+    private static void help(String msg) {
+        if (msg != null) {
+            System.out.println(msg + "\n");
+        }
+        System.out.println("Usage:");
+        System.out.println("java JavaHelloWorld [-window]");
+        System.out.println(" no argument: Hello World on command line");
+        System.out.println(" -window argument: Hello World at GUI Window");
+        System.out.println("Examples:");
+        System.out.println("java JavaHelloWorld           // command line message");
+        System.out.println("java JavaHelloWorld -window   // GUI message");
+        System.out.println("java JavaHelloWorld -blabla   // unknown argument...help");
+    }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        if (args.length <= 0) {
+            System.out.println("Hello World!");
+        } else {
+            if (args[0].equalsIgnoreCase("-window")) {
+                new JavaHelloWorld();
+            } else {
+                help("Missing arguments");
+                System.exit(1);
+            }
+        }
     }
-    
 }

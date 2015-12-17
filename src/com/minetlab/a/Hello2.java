85f7a18c9f5a3fe1d27857ecbe26b4fa6e1df6d6
chlachula <josefch@gmail.com>
2015-12-16 21:10:31 -0600

    Commit from IDEA 15 at home office

---
+++ src/com/minetlab/a/Hello2.java
@@ -0,0 +1,13 @@
+/**
+ * Just Hello
+ */
+package com.minetlab.a;
+/**
+ * Created by JosefCh on 12/16/2015.
+ */
+public class Hello2 {
+    public static void main(String[] args) {
+        Hello2 h = new Hello2();
+        System.out.println("Hello2");
+    }
+}
